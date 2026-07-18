b1943cf457bc43930e270d0f9c392d4bc57043ca
Kirill Kurochkin <kurochkin.k@puzzle-english.com>
2026-07-18 14:50:37 +0600

    исправление 3 PR по заданию 3 первые 2 пункта

--- pages/infinite_scroll_page.py
+++ pages/infinite_scroll_page.py
@@ -1,6 +1,6 @@
 from ui.multi_web_element import MultiWebElement
 from ui.page_actions import PageActions
-from playwright.sync_api import Page
+from playwright.sync_api import Page, TimeoutError as PlaywrightTimeoutError
 
 
 class InfiniteScrollPage:
@@ -17,17 +17,17 @@ class InfiniteScrollPage:
     def get_paragraphs_count(self) -> int:
         return self.paragraphs.count()
 
-    def scroll_down(self, timeout: int = 5000):
+    def scroll_down(self):
         old_count = self.get_paragraphs_count()
         self.page.evaluate("window.scrollTo(0, document.body.scrollHeight)")
 
         try:
             self.page.wait_for_function(
                 f"document.querySelectorAll('.jscroll-added').length > {old_count}",
-                timeout=timeout
+                timeout=5000
             )
             self.get_paragraphs_count()
-        except Exception:
+        except PlaywrightTimeoutError:
             self.page.evaluate("window.scrollTo(0, document.body.scrollHeight)")
 
     def scroll_until(self, target: int, max_attempts: int = 15):
@@ -37,7 +37,7 @@ class InfiniteScrollPage:
             if count >= target:
                 return count
 
-            self.scroll_down(timeout=3000)
+            self.scroll_down()
 
         final_count = self.get_paragraphs_count()
         return final_count
